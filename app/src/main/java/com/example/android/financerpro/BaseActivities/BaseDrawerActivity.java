aff812c6b00e83be96a8fb1a9a0e1a1e64994fbe
Tolga Şen <tolgasen9558@gmail.com>
2017-12-06 12:41:22 -0500

    Branch init

--- app/src/main/java/com/example/android/financerpro/BaseActivities/BaseDrawerActivity.java
+++ app/src/main/java/com/example/android/financerpro/BaseActivities/BaseDrawerActivity.java
@@ -6,17 +6,13 @@ import android.os.Bundle;
 import com.example.android.financerpro.CheckCalculatorActivity;
 import com.example.android.financerpro.MainActivity;
 import com.example.android.financerpro.R;
-
-
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.annotation.LayoutRes;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
